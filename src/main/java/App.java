f563df469a18333af7d00bae555e9d872b8d214a
Sergey Chelyshev <sergey36russia@gmail.com>
2024-11-13 16:23:04 +0300

    create Class Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,22 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld beanCopy =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        Cat beanCatCopy = (Cat) applicationContext.getBean("cat");
+        boolean beanResult = bean == beanCopy;
+        boolean beanCatResult = beanCat == beanCatCopy;
+        System.out.println(beanResult);
+        System.out.println(beanCatResult);
+
+
+
     }
 }
